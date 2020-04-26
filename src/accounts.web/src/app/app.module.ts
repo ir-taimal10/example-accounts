aa150f86bd676eb0ce8071e32e000b3b41cb231a
irtaimal <ir.taimal10@uniandes.edu.co>
2020-04-26 18:41:45 -0500

    impl:BASIC-FRONT, create site info component

--- src/accounts.web/src/app/app.module.ts
+++ src/accounts.web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteInfoComponent } from './site-info/site-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteInfoComponent
   ],
   imports: [
     BrowserModule,
