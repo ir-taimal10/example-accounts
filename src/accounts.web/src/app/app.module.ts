93fca3f2d4be3b2346f336cfd2e492985a0dc903
mfernandezm <fernandezmendezmarcela@gmail.com>
2020-04-26 19:32:36 -0500

    impl: add component, header, footer, login

--- src/accounts.web/src/app/app.module.ts
+++ src/accounts.web/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SiteInfoComponent } from './site-info/site-info.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SiteInfoComponent
+    SiteInfoComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
