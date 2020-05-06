03c75d3f80fd0341de48ffb532fff4301aa04bc9
irtaimal <ir.taimal10@uniandes.edu.co>
2020-05-05 23:21:08 -0500

    initial commit

--- src/accounts/AccountsConfigurationProvider.ts
+++ src/accounts/AccountsConfigurationProvider.ts
@@ -2,6 +2,9 @@ import {Account} from "./Accounts";
 import {configuration} from "./Configuration";
 
 export class AccountsConfigurationProvider {
+    /*@claims()
+    @features()
+    @jwks()*/
     public static getConfiguration() {
         configuration.findAccount = Account.findAccount;
         return configuration;
